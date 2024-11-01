71875331ad63fe163adaac92315d8c31a5f718d3
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 23:42:40 +0200

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1f2937",
+        secondary: "#6b7280",
+        background: "#f3f4f6",
+      },
+    },
   },
   plugins: [],
 };
