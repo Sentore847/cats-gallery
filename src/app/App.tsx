055cd5451e40db732a44175374f9c63daca640a7
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 17:27:45 +0200

    test styles for app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,9 @@
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <main className="bg-red-500">
+      <h1 className="text-yellow-400">App</h1>
+    </main>
+  );
 };
 
 export default App;
