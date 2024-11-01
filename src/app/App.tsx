91e577d7bc1fc30269bf0cb1e5bda09fa8823807
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 18:27:41 +0200

    Added approuter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
+import AppRouter from "./routers/AppRouter";
+
 const App: React.FC = () => {
   return (
-    <main className="bg-red-500">
-      <h1 className="text-yellow-400">App</h1>
-    </main>
+    <>
+      <h1>Cats Gallery</h1>
+      <AppRouter />
+    </>
   );
 };
 
