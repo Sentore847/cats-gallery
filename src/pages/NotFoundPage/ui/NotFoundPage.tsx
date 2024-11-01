04cb3baf54fe375b6b00b77760eb2220a59e7639
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 23:43:40 +0200

    Styled not found page, and added back button

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router";
+
 const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Not Found Page</h1>
+    <div className="text-center">
+      <h1 className="font-bold text-xl">Not Found Page</h1>
+      <button className="font-medium" onClick={() => navigate(-1)}>
+        Go back &rarr;
+      </button>
     </div>
   );
 };
