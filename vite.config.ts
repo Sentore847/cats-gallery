64e89e5afc352cbbd49ca1b42b55d6c12fa1b172
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 18:26:49 +0200

    added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      pages: "/src/pages",
+      features: "/src/features",
+      assets: "/src/assets",
+      hooks: "/src/hooks",
+      styles: "/src/styles",
+    },
+  },
+});
