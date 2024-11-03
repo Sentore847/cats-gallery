fa5f52aa1d12471706d7efb84f11d23229766c52
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-03 13:16:12 +0200

    Added navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
+import { Navbar } from "../widgets/Navbar";
 import AppRouter from "./routers/AppRouter";
 
 const App: React.FC = () => {
   return (
     <>
-      <div className="py-10">
-        <h1 className="font-bold text-3xl text-center">Cats Gallery</h1>
-      </div>
+      <Navbar />
       <AppRouter />
     </>
   );
