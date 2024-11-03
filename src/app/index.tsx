88368a8a798ec005e969aa42d48feeb5be76a873
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-03 13:17:33 +0200

    added context providers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { FavoriteProvider } from "../context/FavoriteContext";
+import { SearchProvider } from "../context/SearchContext";
 import "../styles/index.css";
 import App from "./App";
 
@@ -18,9 +20,13 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <SearchProvider>
+          <FavoriteProvider>
+            <App />
+          </FavoriteProvider>
+        </SearchProvider>
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
