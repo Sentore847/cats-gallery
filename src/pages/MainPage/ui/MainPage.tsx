dd34298eeb36abce4302f376963c57531bfe7e32
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-03 13:20:00 +0200

    exported getData, added contexts

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,69 +1,82 @@
 import { useQuery } from "@tanstack/react-query";
+import { useFavorite } from "../../../context/FavoriteContext";
+import { useSearch } from "../../../context/SearchContext";
 import axios from "axios";
 
 interface Image {
   url: string;
 }
 
-interface Cat {
+export interface Cat {
   id: string;
   name: string;
   description: string;
   image?: Image;
 }
 
-const getData = async () => {
+export const getData = async () => {
   const res = await axios.get(
-    "https://api.thecatapi.com/v1/breeds?api_key=live_UUpJ4dyYshuwcnZacrWPwM0lqNdOn3UgAZk94ZX9F9IeGUblH1tc4JY9MNfIKuo0"
+    "https://api.thecatapi.com/v1/breeds?api_key=live_UUpJ4dyYshuwcnZacrWPwM0lqNdOn3UgAZk94ZX9F9IeGUblH1tc4JY9MNfIKuo0",
   );
-
   return res.data;
 };
 
 const MainPage: React.FC = () => {
+  const { searchTerm } = useSearch();
+
+  const { favorites, toggleFavorite } = useFavorite();
+
   const { data, error, isSuccess } = useQuery<Cat[], Error>({
     queryKey: ["cats"],
     queryFn: getData,
   });
 
-  console.log(error, isSuccess);
-
   if (error) return <div>Error: {error.message}</div>;
   if (!isSuccess || !data) return <div>Loading...</div>;
 
+  const filteredCats = data.filter((cat) =>
+    cat.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold text-center mb-4">Main Page</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-        {data.map((cat) => (
-          <div
-            key={cat.id}
-            className="border rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105  grid grid-rows-[auto,1fr,auto]"
-          >
-            {cat.image ? (
-              <img
-                className="w-full h-48 object-cover"
-                src={cat.image.url}
-                alt={cat.name}
-              />
-            ) : (
-              <div className="w-full h-48 bg-gray-300 flex items-center justify-center">
-                <span>No Image Available</span>
+    <div className="py-4">
+      <h1 className="text-2xl font-semibold mb-4 text-center">Cats</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 px-4">
+        {filteredCats.map((cat) => {
+          const isFavorite = favorites.includes(cat.id);
+          return (
+            <div
+              key={cat.id}
+              className={`cat-card ${isFavorite ? "favorite" : ""}`}
+            >
+              {cat.image ? (
+                <img
+                  className="cat-card-image"
+                  src={cat.image.url}
+                  alt={cat.name}
+                />
+              ) : (
+                <div className="w-full h-48 bg-gray-300 flex items-center justify-center">
+                  <span>No Image Available</span>
+                </div>
+              )}
+              <div className="cat-card-content">
+                <h2 className="cat-card-title">{cat.name}</h2>
+                <p className="cat-card-description">
+                  {cat.description.length > 165
+                    ? `${cat.description.slice(0, 165)}...`
+                    : cat.description}
+                </p>
+                <button
+                  onClick={() => toggleFavorite(cat.id)}
+                  className={`favorite-button ${isFavorite ? "favorited" : ""}`}
+                >
+                  {isFavorite ? "Favorited" : "Add to favorites"}
+                </button>
               </div>
-            )}
-            <div className="p-4 flex flex-col justify-between">
-              <h2 className="text-lg font-semibold">{cat.name}</h2>
-              <p className="text-gray-600">
-                {cat.description.length > 165
-                  ? `${cat.description.slice(0, 165)}...`
-                  : cat.description}
-              </p>
-              <button className="mt-auto w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
-                Add to favorites
-              </button>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
